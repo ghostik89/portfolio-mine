c547f8913b138068e94fe400bef4be478519ff3a
Ekaterina Tarapatina <facktartar@gmail.com>
2021-09-15 10:35:31 +0300

    [init] init repo

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,57 +6,37 @@
  */
 
 import React from "react"
-import { StaticQuery, graphql } from "gatsby"
 
 import { Container, Row, Col } from "react-bootstrap"
 
 import Header from "./header"
-import Navbar from "./navBar"
 
-const Layout = ({ children, pageInfo }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Container fluid className="px-0 main">
-          <Row noGutters className="justify-content-center">
-            <Col>
-              <Header siteTitle={data.site.siteMetadata.title} />
-            </Col>
-          </Row>
-          <Navbar pageInfo={pageInfo} />
-          <Row noGutters>
-            <Col>
-              <Container className="mt-5">
-                <main>{children}</main>
-              </Container>
-            </Col>
-          </Row>
-        </Container>
-        <Container fluid className="px-0">
-          <Row noGutters>
-            <Col className="footer-col">
-              <footer>
-                <span>
-                  © {new Date().getFullYear()}, Built with
-                  {` `}
-                  <a href="https://www.gatsbyjs.org">Gatsby</a>
-                </span>
-              </footer>
-            </Col>
-          </Row>
-        </Container>
-      </>
-    )}
-  />
+const Layout = ({ children }) => (
+  <>
+    <Container fluid className="px-0 main">
+      <Row noGutters className="justify-content-center">
+        <Col>
+          <Header />
+        </Col>
+      </Row>
+      <Row noGutters>
+        <Col>
+          <Container className="mt-5">
+            <main>{children}</main>
+          </Container>
+        </Col>
+      </Row>
+    </Container>
+    <Container fluid className="px-0">
+      <Row noGutters>
+        <Col className="footer-col">
+          <footer>
+            <span>© {new Date().getFullYear()}</span>
+          </footer>
+        </Col>
+      </Row>
+    </Container>
+  </>
 )
 
 export default Layout
